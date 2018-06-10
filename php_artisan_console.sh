03805aab3eac2ce75b32c7423afec43ad2378f0b
Datagit <datagit@yahoo.com>
2018-06-10 22:57:20 +0700

    [Install] laravelcollective/html
     Changes to be committed:
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/app.php
    	modified:   php_artisan_console.sh

--- php_artisan_console.sh
+++ php_artisan_console.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 #http://laravel.com/
 composer create-project laravel/laravel laravel5.6_tutorial
+#start server
 php artisan serve
+#show list command
 php artisan
+#show list routes
 php artisan route:list
 #http://laravel-recipes.com/
 php artisan make:controller UriController
+#install Laravel - Forms
+#https://laravelcollective.com/docs/master/html#installation
+composer require "laravelcollective/html":"^5.4.0"
