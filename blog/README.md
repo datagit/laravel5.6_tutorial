bda1a3a43dec57ee4e2eb784568e0da970586675
Datagit <datagit@yahoo.com>
2018-09-01 17:58:53 +0700

    [Add packgae debugbar]
     Changes to be committed:
    	new file:   README.md
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   routes/web.php
    	new file:   storage/debugbar/.gitignore

---
+++ blog/README.md
@@ -0,0 +1,7 @@
+#### Init
+
+#### Best Practices Examples
+    - https://www.cloudways.com/blog/laravel-tutorials-for-beginners/#LaravelRoutes
+    - http://www.laravelbestpractices.com/#debugging
+#### Common Packages
+    - https://github.com/barryvdh/laravel-debugbar
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,37 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('foo', function () {
+    //using way1:
+    $object = array(1,2,3,);
+    Debugbar::info($object);
+    Debugbar::error('Error!');
+    Debugbar::warning('Watch out…');
+    Debugbar::addMessage('Another message', 'mylabel');
+
+
+    //using way2:
+    try {
+        throw new Exception('foobar');
+    } catch (Exception $e) {
+        Debugbar::addThrowable($e);
+    }
+
+
+    //using way3:
+    // All arguments will be dumped as a debug message
+    $var1 = "Dao Man Dat";
+    $someString = "some string";
+    $intValue = rand(1,10);
+    debug($var1, $someString, $intValue, $object);
+    Debugbar::startMeasure('render','Time for rendering');
+    Debugbar::stopMeasure('render');
+    Debugbar::addMeasure('now', LARAVEL_START, microtime(true));
+    Debugbar::measure('My long operation', function() {
+        // Do something…
+        sleep(2);
+    });
+
+    return 'Hello World';
+});
